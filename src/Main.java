96f5067b1561a061b68c00653c0c0c8788809de3
BTHS <sample@bths.edu>
2026-03-24 11:02:34 -0400

    Initial commit
    Tasks 1 + 2 complete!

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File data = new File("src/movies_data.csv");
+        Scanner scan = null;
+        try {
+            scan = new Scanner(data);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] split = line.split(",");
+            String title = split[0];
+            String cast = split[1];
+            String director = split[2];
+            String overview = split[3];
+            int runtime = Integer.parseInt(split[4]);
+            double userRating = Double.parseDouble(split[5]);
+            Movie movie = new Movie(title, cast, director, overview, runtime, userRating);
+            movies.add(movie);
+        }
+
+        System.out.println("Number of movies: " + movies.size());
+        System.out.println();
+        System.out.println(movies.get(2));
+        System.out.println();
+        System.out.println(movies.get(4998));
+    }
+}
